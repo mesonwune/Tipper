ea5a9ab9a3bf1b5a33a2d58d15e7c5fdd5467e56
Mesoma Esonwune <mesonwune@gmail.com>
2019-01-28 16:31:32 -0800

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Mesoma Esonwune on 1/28/19.
+//  Copyright © 2019 Mesoma Esonwune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
